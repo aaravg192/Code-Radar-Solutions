b79da88619f628327e8587294698883cc7f34b98
Aarav Goyal <aaravgoyal1972006@gmail.com>
2025-02-17 14:53:33 +0530

    Solution to Clear nth Bit of a Number

---
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, n, bit;
+    
+    // Input the number
+    scanf("%d", &num);
+    scanf("%d", &n);
+    
+    // Validate the bit position
+    if (n < 0 || n > 31) {
+        printf("Error: Bit position must be between 0 and 31.\n");
+        return 1; // Exit with error
+    }
+
+    // Extract the nth bit using bitwise shift and AND
+    bit = (num >> n) & 1;
+    if(bit==0) {
+        printf("%d",num)
+    }
+    else{
+        printf("%d",(int)(bit-pow(2,n)));
+    }
+    
+    return 0;
+}
